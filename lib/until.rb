e773d02719b2d8ef8fc09800e3ebde63305dee10
jli063 <jli063@rsdmo.org>
2018-06-19 20:48:42 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,13 +1,2 @@
-def using_until
-        levitation_force = 6
-        loop do 
-          if levitation_force==10
-            break
-          end
-          puts "Wingardium Leviosa"
-          
-          levitation_force=levitation_force+1
-        end  
-    
-end
+
 
